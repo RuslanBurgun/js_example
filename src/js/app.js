961df8e10b5d17c9a9fdd68fe32b5b39392dbf47
Inna <inna.rose2305@gmail.com>
2017-12-03 13:54:44 +0300

    two button

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,19 @@
 import '../css/main.scss';
-console.log(15);
+
+// function readyScript() {
+      var button1 = document.querySelector(".first-button");
+      var button2 = document.querySelector(".second-button");
+      
+      button1.addEventListener("click", function(event) {
+        event.preventDefault();
+        button2.classList.add("second-button");
+          console.log('start');
+          var min = 29;
+          var max = 1000;
+          var rand = min - 0.5 + Math.random() * (max - min + 1);
+          rand = Math.round(rand);
+          button2.innerHTML = rand;
+          console.log('end');
+      }); 
+    
+    // }
