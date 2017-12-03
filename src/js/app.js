3160d8693e85bc3436722c892dc3d52ffce9bda1
Inna <inna.rose2305@gmail.com>
2017-12-03 22:11:44 +0300

    separate stages of function

--- src/js/app.js
+++ src/js/app.js
@@ -3,28 +3,33 @@ import '../css/main.scss';
 document.addEventListener("DOMContentLoaded", function() {
   console.log('start');
 
-function readyScript() {
-      var button1 = document.querySelector(".first-button"); // Получение кнопки из документа
-      var button2 = document.querySelector(".second-button"); 
-      var container1 = document.querySelector(".container1");
-      
-      button1.addEventListener("click", function(event) {
-        event.preventDefault();
-         
-          console.log('start');
-          var min = 29;
-          var max = 1000;
-          var rand = min - 0.5 + Math.random() * (max - min + 1);
-          rand = Math.round(rand);
-          container1.innerHTML = rand;
-          console.log('end');
-      }); 
+  var button1 = document.querySelector(".first-button"); // Получение кнопки из документа
+  var button2 = document.querySelector(".second-button"); 
+  var container1 = document.querySelector(".container1");     
 
-      button2.addEventListener("click", function(event) {
-        event.preventDefault();
-        container1.innerHTML = "";
-    });
+
+  function generate() {
+    button1.addEventListener("click", function(event) {
+      event.preventDefault();
+       
+        console.log('start');
+        var min = 29;
+        var max = 1000;
+        var rand = min - 0.5 + Math.random() * (max - min + 1);
+        rand = Math.round(rand);
+        container1.innerHTML = rand;
+        console.log('end');
+    }); 
   }
-    readyScript();
-  
+
+
+  function clear() {
+    button2.addEventListener("click", function(event) {
+      event.preventDefault();
+      container1.innerHTML = "";
+  });
+  }
+
+  generate();
+  clear();
   });
