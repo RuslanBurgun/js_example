62f24717d738fa43085f4766b17446af8fe99c42
Inna <inna.rose2305@gmail.com>
2017-12-03 21:28:40 +0300

    add clearn button and fix code

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,30 @@
 import '../css/main.scss';
 
-// function readyScript() {
-      var button1 = document.querySelector(".first-button");
-      var button2 = document.querySelector(".second-button");
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('start');
+
+function readyScript() {
+      var button1 = document.querySelector(".first-button"); // Получение кнопки из документа
+      var button2 = document.querySelector(".second-button"); 
+      var container1 = document.querySelector(".container1");
       
       button1.addEventListener("click", function(event) {
         event.preventDefault();
-        button2.classList.add("second-button");
+         
           console.log('start');
           var min = 29;
           var max = 1000;
           var rand = min - 0.5 + Math.random() * (max - min + 1);
           rand = Math.round(rand);
-          button2.innerHTML = rand;
+          container1.innerHTML = rand;
           console.log('end');
       }); 
-    
-    // }
+
+      button2.addEventListener("click", function(event) {
+        event.preventDefault();
+        container1.innerHTML = "";
+    });
+  }
+    readyScript();
+  
+  });
